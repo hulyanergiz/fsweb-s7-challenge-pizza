4e59f1b2f9b05d920dffc159f7f857befb7b2c66
Hülya Nergiz <akin.hulya@outlook.com>
2023-11-09 23:12:39 +0300

    Main adv updates

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -3,7 +3,10 @@ import { Switch, Route } from "react-router-dom";
 import Anasayfa from "../pages/AnaSayfa";
 import SiparisFormu from "../pages/SiparisFormu";
 import SiparisAlindi from "../pages/SiparisAlindi";
+import logo from "../Assets/logo.svg";
+import banner from "../Assets/adv-aseets/adv-form-banner.png";
 import "./Main.css";
+import Footer from "./Footer";
 
 function Main(props) {
   let price = 85.5;
@@ -13,7 +16,11 @@ function Main(props) {
         <Anasayfa />
       </Route>
       <Route path="/pizza">
+        <div className="header-div">
+          <img className="logo" src={logo} alt="logo" />
+        </div>
         <div className="heading-path">
+          <img className="banner" src={banner} alt="pizza" />
           <p className="heading-p">
             <a className="homePage" href="/">
               Anasayfa
@@ -21,25 +28,32 @@ function Main(props) {
             -Seçenekler-<strong>Sipariş Oluştur</strong>
           </p>
         </div>
-        <div className="main">
-          <h3>Position Absolute Acılı Pizza</h3>{" "}
-          {/* idsi name-input olan input olcak */}
-          <div className="digital-container">
-            <div className="price">{price}₺</div>
-            <div className="rating">
-              <div>4.9</div>
-              <div>(200)</div>
+        <div className="main-container-upper">
+          <div className="main-bg">
+            <div className="main-detail">
+              <h3>Position Absolute Acılı Pizza</h3>{" "}
+              <div className="digital-container">
+                <div className="price">{price}₺</div>
+                <div className="rating">
+                  <div>4.9</div>
+                  <div>(200)</div>
+                </div>
+              </div>
+              <p>
+                Frontend Dev olarak hala position:absolute kullanıyorsan bu çok
+                acı pizza tam sana göre.Pizza, domates, peynir ve genellikle
+                çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel
+                olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen,
+                genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan
+                oluşan İtalyan kökenli lezzetli bir yemektir. Küçük pizzaya
+                bazen pizzetta denir.{" "}
+              </p>
             </div>
           </div>
-          <p>
-            Frontend Dev olarak hala position:absolute kullanıyorsan bu çok acı
-            pizza tam sana göre.Pizza, domates, peynir ve genellikle çeşitli
-            diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun
-            ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle
-            yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan
-            kökenli lezzetli bir yemektir. Küçük pizzaya bazen pizzetta denir.{" "}
-          </p>
-          <SiparisFormu price={price} />
+          <div className="main-container-lower">
+            <SiparisFormu price={price} />
+          </div>
+          <Footer />
         </div>
       </Route>
       <Route path="/siparisalindi">
